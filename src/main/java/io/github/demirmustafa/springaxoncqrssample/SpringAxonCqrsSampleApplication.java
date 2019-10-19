9d6542ba164359b6f2f5226effb21bec6a32365b
Mustafa Demir <mustafa.demir@b2cdirect.com>
2019-10-19 15:09:05 +0300

    scaffolding

---
+++ src/main/java/io/github/demirmustafa/springaxoncqrssample/SpringAxonCqrsSampleApplication.java
@@ -0,0 +1,13 @@
+package io.github.demirmustafa.springaxoncqrssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAxonCqrsSampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAxonCqrsSampleApplication.class, args);
+    }
+
+}
